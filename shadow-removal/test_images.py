f6525ed5824277f58f3a52ed6bf4ba3c7cf7ddbd
bronte-audere <bronte@auderenow.org>
2023-10-09 11:00:40 -0700

    Apply shadows and test classification

---
+++ shadow-removal/test_images.py
@@ -0,0 +1,86 @@
+# Bronte Sihan Li, 2023
+
+import argparse
+import torch
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as transforms
+from torchmetrics import ConfusionMatrix
+from resnet import ResNet
+import matplotlib.pyplot as plt
+import numpy as np
+
+parser = argparse.ArgumentParser(description="Test GTSRB images")
+parser.add_argument(
+    "--checkpoint_path",
+    type=str,
+    default="checkpoints/model_35.pth",
+    help="The path to the model checkpoint",
+)
+
+parser.add_argument(
+    "--test_dir",
+    type=str,
+    default="adv_img/GTSRB",
+    help="The path to the test images",
+)
+
+args = parser.parse_args()
+checkpoint_path = args.checkpoint_path
+test_dir = args.test_dir
+
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Load the model
+model = ResNet().to(device)
+state_dict = torch.load(checkpoint_path, map_location=device)
+model.load_state_dict(state_dict)
+
+# Define the test set
+data_transforms = transforms.Compose(
+    [
+        transforms.Resize((48, 48)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.3337, 0.3064, 0.3171), (0.2672, 0.2564, 0.2629)),
+    ]
+)
+test_set = ImageFolder(root=test_dir, transform=data_transforms)
+
+# Define the test loader
+test_loader = torch.utils.data.DataLoader(
+    test_set,
+    batch_size=1,
+    shuffle=False,
+)
+
+if __name__ == '__main__':
+    # Random seed
+    torch.manual_seed(0)
+
+    cm = ConfusionMatrix(num_classes=43, task='multiclass')
+
+    # Test the model
+    model.eval()
+    correct = 0
+    for data, target in test_loader:
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        pred = torch.argmax(output, dim=1)
+        correct += pred.eq(target.view_as(pred)).cpu().sum()
+        cm.update(pred, target)
+
+    # Produce confusion matrix
+    print('Confusion matrix:')
+    print(cm.compute().numpy())
+    # Plot confusion matrix
+    confusion_matrix = cm.compute().numpy()
+    plt.imshow(confusion_matrix, cmap=plt.cm.Blues)
+    plt.xlabel('Predicted labels')
+    plt.ylabel('True labels')
+    plt.xticks(np.range(43))
+    plt.yticks(np.range(43))
+    plt.title('Confusion matrix')
+    plt.colorbar()
+    plt.savefig('confusion_matrix.png')
+
+    print('Test accuracy: {:.2f}%'.format(100.0 * correct / len(test_loader.dataset)))
